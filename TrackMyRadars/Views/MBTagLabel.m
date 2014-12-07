d40e03e363e287dfc3c8f9b8ac9c4c1d95dd77a7
Maria Bernis <mariabernis.clavijo@gmail.com>
2014-12-07 22:31:30 +0100

    Radar number, Project name

---
+++ TrackMyRadars/Views/MBTagLabel.m
@@ -0,0 +1,21 @@
+//
+//  MBTagLabel.m
+//  TrackMyRadars
+//
+//  Created by Maria Bernis on 07/12/14.
+//  Copyright (c) 2014 mariabernis. All rights reserved.
+//
+
+#import "MBTagLabel.h"
+
+@implementation MBTagLabel
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
