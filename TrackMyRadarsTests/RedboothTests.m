8ba8562e84b5c5699a83baf5237532faa0733ed5
Maria Bernis <mariabernis.clavijo@gmail.com>
2014-12-08 19:40:37 +0100

    API redirect URI updated

--- TrackMyRadarsTests/RedboothTests.m
+++ TrackMyRadarsTests/RedboothTests.m
@@ -12,13 +12,12 @@
 #define RB_API_BASE_URL     @"https://redbooth.com"
 #define RB_API_CLIENT       @"56b67669c4c3251ca5aa45f1509526ef8eddff006956de014daa8b035b24dbd8"
 #define RB_API_SECRET       @"6c97be47b73147bb7af8ff3f1ddeb04aeda1b1b62666744aa9b3622105c1301f"
-#define RB_API_CALLBACK_URL @"http://www.laurarocacodina.com/authcode.html"
 
 #define KEY_ACCESS_TOKEN    @"access_token"
 #define KEY_TOKEN_EXPIRES   @"access_token_expires"
 #define KEY_REFRESH_TOKEN   @"refresh_token"
 
-#define RB_API_HOST     @"redbooth.com"
+#define RB_API_HOST         @"redbooth.com"
 
 
 SPEC_BEGIN(APIAuthorization)
